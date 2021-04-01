15b5285970e118a48e80405813917bb81b2cea28
kitaye <44532933+kitaye@users.noreply.github.com>
2021-04-01 23:22:34 +0300

    onhover effects to header

---
+++ script.js
@@ -0,0 +1,13 @@
+function menuToggle() {
+  const toggle = document.querySelector('.header__nav-toggle');
+  const menu = document.querySelector('.menu');
+
+  toggle.addEventListener('click', function() {
+    this.classList.toggle('opened');
+    menu.classlist.toggle('open');
+  })
+
+
+}
+
+menuToggle();
